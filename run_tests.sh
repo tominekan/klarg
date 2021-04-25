051279861585fd3923096c4f672558cc1ccad419
Tomi Adenekan <tominekan@outlook.com>
2021-04-24 17:58:00 -0700

    Updated Test running to fit multi-file testing

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,20 @@
-# Test the python program
-python test_klarg.py --some-number 10 --not-number 1a --number-no-args  -n
+# Test normal klarg functions
+echo "--------------- Testing Normal Klarg Functions ---------------"
+echo ""
+echo ""
+python3.8 test_klarg.py --some-number 10 --not-number 1a --number-no-args  -n
+echo ""
+echo ""
 
+# Test klarg.command functions
+echo "--------------- Testing klarg.command functions ---------------"
+echo ""
+echo ""
+python3.8 test_klarg_command.py --this-should-not-work test --some-number 10 --not-number 1a --number-no-args  -n
+echo ""
+echo ""
+
+echo "--------------- End Tests ---------------"
 # Remove __pycache__ folder (for some reason, __pycache__ folders deeply annoy me)
 rm -rfv __pycache__
 
